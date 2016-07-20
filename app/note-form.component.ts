9af50d69740230c9c0c2c229988d3c198658969c
Matthew Harvey <software@matthewharvey.net>
2016-07-20 16:23:14 +0930

    Add icons to buttons.

--- app/note-form.component.ts
+++ app/note-form.component.ts
@@ -4,20 +4,25 @@ import { Button, InputTextarea } from 'primeng/primeng';
 import { Note } from './note';
 import { NoteService } from './note.service';
 
-// TODO Use font awesome on button labels, and have distinct background colours
-// on buttons depending on label.
+// TODO Have distinct background colours on buttons depending on label.
 @Component({
   selector: 'my-note-form',
   template: `
     <div class="wrapper">
       <textarea #contentInput class="js-content-input" autofocus
         pInputTextarea autoResize="autoResize" [rows]=3 [cols]=60
-        [readonly]="!editable" [(ngModel)]="note.content"></textarea>
+        [readonly]="note.persisted() && !beingEdited" [(ngModel)]="note.content"></textarea>
       <div class="button-wrapper">
-        <button pButton type="button" (click)="onEditOrSave()"
-          [disabled]="!note.valid()" label="{{saveButtonLabel()}}"></button>
-        <button *ngIf="note.persisted()" pButton type="button" (click)="onCancelOrDelete()"
-          label="{{deleteButtonLabel()}}"></button>
+        <button *ngIf="creatable()" pButton type="button" (click)="onCreate()" label="Add"
+          [disabled]="!note.valid()" icon="fa-plus"></button>
+        <button *ngIf="editable()" pButton type="button" (click)="onEdit()" label="Edit"
+          (click)="onEdit()" icon="fa-pencil"></button>
+        <button *ngIf="updatable()" pButton type="button" (click)="onUpdate()" label="Save"
+          [disabled]="!note.valid()" icon="fa-save"></button>
+        <button *ngIf="deletable()" pButton type="button" (click)="onDelete()"
+          label="Delete" icon="fa-trash-o"></button>
+        <button *ngIf="cancelable()" pButton type="button" (click)="onCancel()"
+          label="Cancel" icon="fa-ban"></button>
       </div>
     </div>
   `,
@@ -30,10 +35,16 @@ import { NoteService } from './note.service';
     textarea,
     .button-wrapper {
       display: table-cell;
+      padding-top: 0.1em;
       vertical-align: top;
     }
     .button-wrapper button {
-      min-width: 5.75em;
+      font-size: 0.9em;
+      width: 6.75em;
+      margin-bottom: 0.25em;
+    }
+    .button-wrapper button span {
+      font-weight: normal;
     }
     .ui-inputtext {
       background-color: hsla(60, 75%, 97.5%, 1);
@@ -63,7 +74,7 @@ export class NoteFormComponent {
   private oldContent: string;
 
   @Input()
-  private editable = false;
+  private beingEdited = false;
 
   @ViewChild('contentInput')
   private contentInput: ElementRef;
@@ -74,48 +85,58 @@ export class NoteFormComponent {
     this.renderer.invokeElementMethod(this.contentInput.nativeElement, 'focus', []);
   }
 
-  saveButtonLabel(): string {
-    if (this.editable) {
-      return (this.note.persisted() ? 'Save' : 'Add');
-    }
-    return 'Edit';
+  creatable(): boolean {
+    return !this.note.persisted();
   }
 
-  deleteButtonLabel(): string {
-    return (this.editable ? 'Cancel' : 'Delete');
+  deletable(): boolean {
+    return !this.beingEdited && this.note.persisted();
   }
 
-  onEditOrSave(): void {
-    if (this.note.persisted()) {
-      // TODO Update properly with this.noteService.
-      this.noteService.saveAll();  // TODO Will become async at some point.
-      this.editable = !this.editable;
-      if (this.editable) {
-        this.oldContent = this.note.content;  // save old content so we can undo if needed
+  cancelable(): boolean {
+    return this.beingEdited && this.note.persisted();
+  }
+
+  editable(): boolean {
+    return !this.beingEdited && this.note.persisted();
+  }
+
+  updatable(): boolean {
+    return this.beingEdited && this.note.persisted();
+  }
+
+  onCreate(): void {
+    var oldNote = this.note;
+    this.note = new Note();
+    // TODO Handle error once .addNote might fail.
+    this.noteService.addNote(oldNote).then(id => {
+      if (id < 1) {
+        // Adding of note was unsuccessful
+        // TODO We probably want to display an error message here too.
+        this.note = oldNote;
+      } else {
         this.focusInput();
       }
-    } else {
-      var oldNote = this.note;
-      this.note = new Note();
-      // TODO Handle error once .addNote might fail.
-      this.noteService.addNote(oldNote).then(id => {
-        if (id < 1) {
-          // Adding of note was unsuccessful
-          // TODO We probably want to display an error message here too.
-          this.note = oldNote;
-        } else {
-          this.focusInput();
-        }
-      });
-    }
+    });
   }
 
-  onCancelOrDelete(): void {
-    if (this.editable) {
-      this.note.content = this.oldContent;
-      this.editable = false;
-    } else {
-      this.noteService.deleteNote(this.note);
-    }
+  onEdit(): void {
+    this.beingEdited = true;
+    this.oldContent = this.note.content;
+    this.focusInput();
+  }
+
+  onUpdate(): void {
+    this.beingEdited = false;
+    this.noteService.saveAll();  // TODO Will become async at some point.
+  }
+
+  onCancel(): void {
+    this.note.content = this.oldContent;
+    this.beingEdited = false;
+  }
+
+  onDelete(): void {
+    this.noteService.deleteNote(this.note);
   }
 }
